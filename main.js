ea02f80e667a2766036550266d853a29f3b32747
AmbaCaramba <4232478+AmbaCaramba@users.noreply.github.com>
2024-05-25 23:18:20 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/build/three.module.js';
+import * as THREE from 'three';
 
 // Create the scene, camera, and renderer
 const scene = new THREE.Scene();
 // Create the card geometry and material
-const cardGeometry = new THREE.BoxGeometry( 5, 0.1, 3, 2 );
-const cardMaterial = new THREE.MeshBasicMaterial( {
-  color: 'purple',
+const textureloader = new THREE.TextureLoader();
+const cardGeometry = new THREE.BoxGeometry( 3, 5, 0.05);
+const cardMaterial = [new THREE.MeshBasicMaterial( {
+  color: 'white',
+} ),
+new THREE.MeshBasicMaterial( {
+  color: 'white',
+} ),
+new THREE.MeshBasicMaterial( {
+  color: 'white',
   //wireframe: true
-} );
+  // map: textureloader.load('./images/card.jpg'),
+} ),
+new THREE.MeshBasicMaterial( {
+  color: 'white',
+  //wireframe: true
+  // map: textureloader.load('./images/card.jpg'),
+} ),
+new THREE.MeshBasicMaterial( {
+  color: 'white',
+  //wireframe: true
+  // map: textureloader.load('./images/card.jpg'),
+} ),
+new THREE.MeshBasicMaterial( {
+  // color: 'purple',
+  //wireframe: true
+  map: textureloader.load('./images/card.jpg'),
+} ),
+];
 
 // Create the card mesh and add it to the scene
 const cardMesh = new THREE.Mesh( cardGeometry, cardMaterial );
 cardMesh.position.set( 0, 0, 0 );
-cardMesh.rotation.z = Math.PI / 2; // rotate the card to stand upright
+cardMesh.rotation.y = Math.PI / 2; // rotate the card to stand upright
 scene.add( cardMesh );
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight );
@@ -21,13 +45,28 @@ scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('.canvas'),
-  antialias: true
+  antialias: false
 });
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 // Add a light source
-const light = new THREE.DirectionalLight( 0xffffff, 1 );
+const light = new THREE.DirectionalLight( 0xffaffa, 1 );
 scene.add( light );
+
+const raycaster = new THREE.Raycaster();
+const handleClick = (event) => {
+  const pointer = new THREE.Vector2();
+  pointer.x = (event.clientX / window.innerWidth) *2 -1;
+  pointer.y = -(event.clientX / window.innerHeight) *2 +1;
+  raycaster.setFromCamera(pointer, camera);
+  const intersections = raycaster.intersectObjects([cardMesh]);
+
+  for(let i =0; i< intersections.length;i+=1){
+    intersections[i].object.material[4].color.set('purple');
+  }
+};
+
+window.addEventListener('click', handleClick);
 
 
 // Create the animation function
