4c3d0d4822a1d5be24b93dbdc29d7cd220a5dd70
AmbaCaramba <4232478+AmbaCaramba@users.noreply.github.com>
2024-05-25 23:58:50 +0500

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ new THREE.MeshBasicMaterial( {
 new THREE.MeshBasicMaterial( {
   // color: 'purple',
   //wireframe: true
-  map: textureloader.load('./card.jpg'),
+  map: textureloader.load('card.jpg'),
 } ),
 ];
 
